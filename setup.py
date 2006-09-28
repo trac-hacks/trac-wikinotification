8eba567671168f3cc81bd23ed7663fb24ac7a88e
Pedro Algarvio <ufs@ufsoft.org>
2006-09-28 23:57:49 +0100

    [svn r7] Minor changes on ReST table.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# $Id: setup.py 6 2006-09-28 22:53:31Z s0undt3ch $
+# $Id: setup.py 7 2006-09-28 22:57:49Z s0undt3ch $
 
 from setuptools import setup, find_packages
 
@@ -22,6 +22,7 @@ You can find more info on the
 feature requests should go to.
 
 
+
 Enabling the Plugin
 -------------------
 It's as simple as::
@@ -30,30 +31,32 @@ It's as simple as::
    wikinotification.* = enabled
 
 
+
 Available Config Options
 ------------------------
 These are the options available to include on your *trac.ini*.
 
-=====================  ====================  ===========
-**Config Setting**     **Default Value**     **Explanation**
----------------------  --------------------  -----------
+=====================  ====================  ================================
+ **Config Setting**     **Default Value**     **Explanation**
+---------------------  --------------------  --------------------------------
 *redirect_time*        5 (in secconds)       The default secconds a redirect should take when
                                              watching/un-watching a wiki page.
                                              This value is also definable per user, ie, user
                                              is able to configure this, of course for himself.
----------------------  --------------------  -----------
+---------------------  --------------------  --------------------------------
 *smtp_always_bcc*      *empty*               Email address(es) to always send notifications to,
                                              addresses do not appear publicly (Bcc:).
----------------------  --------------------  -----------
+---------------------  --------------------  --------------------------------
 *smtp_always_cc*       *empty*               Email address(es) to always send notifications to,
                                              addresses can be see by all recipients (Cc:).
----------------------  --------------------  -----------
+---------------------  --------------------  --------------------------------
 *smtp_from*            trac.wiki\@localhost  Sender address to use in notification emails.
----------------------  --------------------  -----------
+---------------------  --------------------  --------------------------------
 *use_public_cc*        False                 Recipients can see email addresses of other
                                              CC'ed recipients. If this option is
                                              disabled(the default), recipients are put on BCC.
-=====================  ====================  ===========
+=====================  ====================  ================================
+
 
 
 Download and Installation
