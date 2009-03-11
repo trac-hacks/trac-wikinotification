48afeb9afa5a25c16a3efa062274e0a6ff64d88e
Pedro Algarvio <ufs@ufsoft.org>
2009-03-11 21:01:57 +0000

    [svn r75] Typo correction.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# $Id: setup.py 63 2008-03-06 00:08:29Z s0undt3ch $
+# $Id: setup.py 75 2009-03-11 21:01:57Z s0undt3ch $
 
 import re
 from setuptools import setup, find_packages
@@ -13,36 +13,38 @@ SUMMARY = "Trac Plugin to allow email notification of changes on wiki pages"
 HOME_PAGE = 'http://wikinotification.ufsoft.org'
 LICENSE = 'BSD'
 
-setup(name=PACKAGE,
-      version=VERSION,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      url=HOME_PAGE,
-      download_url='http://python.org/pypi/TracWikiNotification',
-      description=SUMMARY,
-      long_description=re.sub(r'(\.\.[\s]*[\w]*::[\s]*[\w+]*\n)+', r'::\n', open('README.txt').read()),
-      license=LICENSE,
-      platforms="OS Independent - Anywhere Python and Trac >=0.11 is known to run.",
-      install_requires = ['Trac>0.10.99', 'Genshi>=0.5dev-r766'],
-      packages=find_packages(),
-      package_data={'WikiNotification': ['templates/*.html', 'templates/*.txt']},
-      entry_points = {
-          'trac.plugins': [
-              'wikinotification = WikiNotification',
-          ]
-      },
-      keywords = "trac plugin wiki notification",
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Web Environment',
-          'Framework :: Trac',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Text Processing',
-          'Topic :: Utilities',
-          'Topic :: Internet :: WWW/HTTP',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-      ]
-     )
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=HOME_PAGE,
+    download_url='http://python.org/pypi/TracWikiNotification',
+    description=SUMMARY,
+    long_description=re.sub(r'(\.\.[\s]*[\w]*::[\s]*[\w+]*\n)+', r'::\n',
+                            open('README.txt').read()),
+    license=LICENSE,
+    platforms="OS Independent - Anywhere Python and Trac >=0.11 is known to run.",
+    install_requires = ['Trac>0.10.99', 'Genshi>=0.5dev'],
+    packages=find_packages(),
+    package_data={'WikiNotification': ['templates/*.html', 'templates/*.txt']},
+    entry_points = {
+        'trac.plugins': [
+            'wikinotification = WikiNotification',
+        ]
+    },
+    keywords = "trac plugin wiki notification",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Trac',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
